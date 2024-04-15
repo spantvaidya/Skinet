138f65de6ad39eb33c13387d5f84d118129fa27c
Sameer Pantvaidya <sameer.pantvaidya@cons.amplifon.com>
2024-04-15 19:02:40 +0530

    Skinet Secrtion 1 changes

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController(StoreContext context) : ControllerBase
+    {
+        private readonly StoreContext _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+
+
+}
