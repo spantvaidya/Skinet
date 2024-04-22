ee6e6d6a13edfe9110247b7128b8d96c5e3c2c49
sameer <spantvaidya@deloitte.com>
2024-04-22 22:59:45 +0530

    End of Section 8.1 Http client binding

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Skinet AngularApp';
+export class AppComponent implements OnInit{
+  title = 'Skinet';
+products: any[]=[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7151/api/Products?pageSize=58').subscribe({
+      next: (response : any) => this.products = response.data, //what to do 
+      error:error => console.log(error), //what to do if error
+      complete:() => {
+        console.log('request completed');
+        console.log('extra statement');
+      }
+    })
+  }
 }
+
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    BrowserAnimationsModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
